e951bb65c9ba7b9b0809d6f60752421dfb9a8206
timofeeva-olya <olya.sinyakova.20.02@mail.ru>
2024-08-22 10:04:58 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+my_dict = {'Olya':2002, 'Margarita':2003, 'Antonina':2002}
+print(my_dict)
+print(my_dict["Olya"])
+my_dict.update({'Katya':2005,
+                'Kristina':2005})
+print(my_dict)
+del my_dict['Olya']
+print(my_dict)
+my_set= {35, 35, 23,23, 'Mom', "Mom",  True}
+print(my_set)
+my_set.update({12, 'P'})
+print(my_set)
+my_set.discard(35)
+print(my_set)
+
